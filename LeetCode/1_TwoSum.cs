767f0047b42e8ab29283f1c15ffec62ebf6b88c6
lucasquin <lucaslquintino@gmail.com>
2023-10-10 23:37:24 -0300

    TwoSum

---
+++ LeetCode/1_TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace LeetCode;
+
+public partial class LeetCodeSolution
+{
+    public int[] TwoSum()
+    {
+        // nums = [2,7,11,15], target = 9
+        // nums = [3,2,4], target = 6
+
+        int[] arr = new int[] { 2, 7, 11, 15 };
+        int alvo = 9;
+
+        // Pegar um indice,
+        // Ir somando com os outros, se der a soma, break
+        // Se não, pegar o proximo e ir somando
+
+        List<int> result = new();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var next = i + 1;
+            var sum = arr[i] + next;
+
+            if (sum == alvo)
+            {
+                result.Add(arr[i]);
+                result.Add(next);
+                break;
+            }
+        }
+
+        return arr;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCode;
+
+LeetCodeSolution result = new();
+
+Console.WriteLine(result.TwoSum());
