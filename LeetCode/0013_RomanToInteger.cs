e130db0e5cc2ec4d6cc6390b6397fc339acda72f
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-10-17 08:54:13 -0300

    https://leetcode.com/problems/roman-to-integer/ - 17/10/2023

---
+++ LeetCode/0013_RomanToInteger.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode;
+
+public partial class Solution
+{
+    // https://leetcode.com/problems/roman-to-integer/
+    // 17/10/2023
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> num = new()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        var chars = s.ToCharArray();
+        int result = 0;
+
+        for (int i = 0; i < chars.Length - 1; i++)             
+        {
+            int next = num.First(c => c.Key == chars[i + 1]).Value;
+            int current = num.First(c => c.Key == chars[i]).Value;
+
+            result += (next > current ? -1 : 1) * current;       
+        }
+
+        return result + num.First(c => c.Key == chars[^1]).Value;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,14 +9,14 @@ long startMemory = Process.GetCurrentProcess().WorkingSet64;
 
 // Start
 
-int[] x = { 2, 5, 5, 11 };
-solution.IsPalindrome(121);
+string input = "III";
+var result = solution.RomanToInt(input);
 
 // End
 
 long endMemory = Process.GetCurrentProcess().WorkingSet64;
 stopwatch.Stop();
 long memoryUsed = startMemory - endMemory;
-Console.WriteLine("Result: ");
+Console.WriteLine($"Result: {result}");
 Console.WriteLine($"Memory used: {memoryUsed / (1024.0 * 1024.0)} MB");
 Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms");
